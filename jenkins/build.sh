9d0201f100082b9db16f51583e16a470bd1360ed
Henry Robinson <henry@cloudera.com>
2015-05-11 10:52:27 -0700

    Initial commit; some changes to directory names

---
+++ jenkins/build.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+# This script builds Impala and runs all tests.
+
+# Set up common environment
+source ${WORKSPACE}/repos/impala-jenkins-scripts/jenkins/jenkins-preamble.sh
+
+# Enable debug logging in Jenkins.
+# TODO: Look into removing this if it does not have much value.
+set -x
+# Cleanup old logs, snapshots and tarballs.
+rm -rf ${IMPALA_HOME}/cluster_logs/
+rm -f ${WORKSPACE}/cluster_logs*tar.gz
+rm -f ${WORKSPACE}/hive_impala_dump_*.txt
+# Make the be testing directory.
+mkdir -p be/Testing/
+
+# Set defaults.
+: ${EXPLORATION_STRATEGY:=core}
+: ${BUILD_TYPE:=""}
+: ${SKIP_METADATA_LOAD:=false}
+: ${SKIP_FULL_DATA_LOAD:=true}
+BUILDALL_ARGS=""
+RET_VAL=0
+case $BUILD_TYPE in
+  ASAN)
+    BUILDALL_ARGS+=("-asan")
+    BUILDALL_ARGS+=("-skiptests")
+    export ASAN_OPTIONS="handle_segv=0"
+    ;;
+  HEAPCHECK)
+    export HEAPCHECK=normal
+    ;;
+esac
+
+if [[ "$SKIP_FULL_DATA_LOAD" = "true" ]]; then
+  ${IMPALA_HOME}/bin/build_thirdparty.sh -noclean
+  if [[ "$SKIP_METADATA_LOAD" = "true" ]]; then
+    BUILDALL_ARGS+=("-format_cluster")
+    BUILDALL_ARGS+=("-snapshot_file ${IMPALA_SNAPSHOT_FILE}")
+    BUILDALL_ARGS+=("-metastore_snapshot_file ${METASTORE_SNAPSHOT_FILE}")
+  else
+    # Reload the metadata.
+    BUILDALL_ARGS+=("-format")
+    BUILDALL_ARGS+=("-snapshot_file ${IMPALA_SNAPSHOT_FILE}")
+  fi
+else
+  BUILDALL_ARGS+=("-format")
+  BUILDALL_ARGS+=("-testdata")
+fi
+
+if ! ${IMPALA_HOME}/buildall.sh ${BUILDALL_ARGS[@]}; then
+  echo "buildall.sh ${BUILDALL_ARGS[@]} failed."
+  RET_VAL=1
+fi
+
+# ASAN runs only a curated set of tests.
+if [[ "$BUILD_TYPE" = "ASAN" && $RET_VAL -eq 0 ]]; then
+  if ! FE_TEST=false JDBC_TEST=false\
+    $IMPALA_HOME/bin/run-all-tests.sh -e $EXPLORATION_STRATEGY; then
+    RET_VAL=1
+  fi
+fi
+
+# If the build was successful, kill all the running services. This is to help workaround a
+# Jenkins bug which causes artifact archiving to fail if files continue to be modified.
+# Skip this step if the build fails, because it might be useful to keep the services up for
+# debugging.
+if [ ${RET_VAL} -eq 0 ]; then
+  ${IMPALA_HOME}/testdata/bin/kill-all.sh
+fi
+
+# Get a UUID for the jenkins job to use as a prefix for archived artifacts. This is useful
+# to distinguish artifact names when looking at them locally.
+if [[ "$BUILD_TYPE" = "ASAN" || "$BUILD_TYPE" = "HEAPCHECK" ]]; then
+  # A Jenkins Matrix build JOB_NAME environment variable includes the job name plus the
+  # current vector values, separated by front slashes. To get the actual job name, strip
+  # out everything up to the first front slash.
+  JOB_UUID=`cut -d "/" -f1 <<< ${JOB_NAME}`-${BUILD_TYPE}-${BUILD_NUMBER}
+else
+  JOB_UUID=${JOB_NAME}-${BUILD_NUMBER}
+fi
+
+CLUSTER_LOG_TARBALL=cluster_logs-${JOB_UUID}.tar.gz
+if ! tar -czf ${WORKSPACE}/$CLUSTER_LOG_TARBALL ${IMPALA_HOME}/cluster_logs; then
+  echo "Could not create a tarball of the cluster logs."
+fi
+
+# Store the psql dump of the metastore.
+pg_dump -U hiveuser hive_impala > ${WORKSPACE}/hive_impala_dump_${JOB_UUID}.txt
+exit ${RET_VAL}
---
+++ jenkins/jenkins-preamble.sh
@@ -0,0 +1,159 @@
+#!/bin/bash
+# Sets up a common environment for Jenkins builds.
+# Once complete, pwd will be ${IMPALA_HOME}, and bin/impala-config.sh will have been
+# sourced.
+echo "********************************************************************************"
+echo " Building ${JOB_NAME} #${BUILD_NUMBER} "
+echo " Node: ${NODE_NAME} / `hostname`"
+echo " Branch: ${GIT_BRANCH}@${GIT_COMMIT}"
+echo " ${BUILD_URL}"
+echo " Path: ${WORKSPACE}"
+echo "********************************************************************************"
+
+echo ">>> Setting up Impala Jenkins environment..."
+echo ">>> Mounting toolchain"
+. /mnt/toolchain/toolchain.sh # Sets, among other things, THRIFT_HOME
+
+# Don't exit automatically if any pipeline command returns non-0
+set +e
+# Make this verbose.
+set -x
+
+export IMPALA_HOME=$WORKSPACE/repos/Impala
+export IMPALA_AUX_TEST_HOME=
+export IMPALA_BUILD_SCRIPTS_HOME=${WORKSPACE}/repos/impala-jenkins-scripts/
+export IMPALA_LZO=$WORKSPACE/repos/Impala-lzo
+export HADOOP_LZO=$WORKSPACE/repos/hadoop-lzo
+export TARGET_FILESYSTEM=${TARGET_FILESYSTEM-"hdfs"}
+export S3_BUCKET=${S3_BUCKET-}
+export FILESYSTEM_PREFIX=""
+export LLVM_HOME=/opt/toolchain/llvm-3.3
+export PATH=$LLVM_HOME/bin:$THRIFT_HOME/bin:$PATH
+export PIC_LIB_PATH=/opt/toolchain/impala_3rdparty-0.5
+
+# If the target filesystem is s3, we need the following information:
+# - A valid S3 bucket.
+# - A valid aws access key.
+# - A valid aws secret key.
+# The aws keys are used to populate core-site.xml and enable the hadoop client to talk to
+# s3. They're also used as the defaults for Amazon's aws commandline tool.
+# Also note that the s3 buckets must be set up by the user to whom the aws keys belong.
+if [ "${TARGET_FILESYSTEM}" = "s3" ]; then
+  set -e
+  # Do some validation checks.
+  # If either of the access keys are not provided exit.
+  if [[ "${AWS_ACCESS_KEY_ID}" = "" || "${AWS_SECRET_ACCESS_KEY}" = "" ]]; then
+    echo "Both AWS_ACCESS_KEY_ID and AWS_SECRET_KEY_ID need to be set"
+    echo " and belong to the owner of the s3 bucket in order to access the file system"
+    exit 1
+  fi
+  # Check if the s3 bucket is NULL.
+  if [ "${S3_BUCKET}" = "" ]; then
+    echo "The ${S3_BUCKET} cannot be an empty string for s3"
+    exit 1;
+  fi
+  echo ">>> Checking access to S3 bucket: ${S3_BUCKET}"
+  aws s3 ls "s3://${S3_BUCKET}/"
+  echo ">>> Access successful"
+  # At this point, we've verified that:
+  #   - All the required environment variables are set.
+  #   - We are able to talk to the s3 bucket with the credentials provided.
+  export AWS_ACCESS_KEY_ID
+  export AWS_SECRET_ACCESS_KEY
+  export FILESYSTEM_PREFIX="s3a://${S3_BUCKET}"
+  set +e
+fi
+
+TEST_WAREHOUSE_SNAPSHOT_DIR=${WORKSPACE}/testdata/test-warehouse-SNAPSHOT/
+METASTORE_SNAPSHOT_DIR=${WORKSPACE}/testdata/hive_metastore_snapshot
+rm -rf ${METASTORE_SNAPSHOT_DIR}
+mkdir ${METASTORE_SNAPSHOT_DIR}
+
+echo ">>> Building hadoop-lzo"
+cd $HADOOP_LZO
+if $CLEAN; then
+  git clean -dfx && git reset --hard HEAD
+fi
+ant package || { echo "building hadoop-lzo failed"; exit 1; }
+
+pushd $IMPALA_HOME
+if $CLEAN; then
+  echo ">>> Cleaning workspace"
+  git clean -dfx && git reset --hard HEAD
+fi
+
+# Get rid of old results.
+rm -rf tests/results
+# Nuke /tmp/hadoop-jenkins, which some tests leave behind.
+rm -rf /tmp/hadoop-jenkins
+# Nuke the cluster_logs directory, which is occasionally left behind.
+rm -rf cluster_logs
+
+# Find all java processes by this user that aren't slave.jar-related, and if those
+# proceses' parents are 1, kill them to death.
+(ps -fe -u $USER |grep java|grep -v grep |grep -v "slave.jar" |\
+ awk '{ if ($3 ~ "1") { print $2 } }'|xargs kill -9)
+
+# Enable core dumps
+ulimit -c unlimited
+
+# Create a link to the actual data files on the machine
+mkdir -p testdata/impala-data
+ln -s /data/1/workspace/impala-data/* testdata/impala-data/
+
+. bin/impala-config.sh &> /dev/null
+if [ "$CDH_MAJOR_VERSION" != "4" ]; then
+  # CDH5+ requires Java 7. JAVA7_HOME should always be set by toolchain.sh.
+  # Also clear the LD_* variables so they will pick up the new Java version.
+  export JAVA_HOME=$JAVA7_HOME
+  export JAVA64_HOME=$JAVA7_HOME
+  export PATH=$JAVA_HOME/bin:$PATH
+  export LD_LIBRARY_PATH=""
+  export LD_PRELOAD=""
+  # Re-source impala-config since JAVA_HOME has changed.
+  # TODO: Split up impala-config so this step isn't needed?
+  . bin/impala-config.sh &> /dev/null
+fi
+popd
+
+# Unset NUM_CONCURRENT_TESTS for Jenkins, we want the highest degree of parallelism
+# possile.
+unset NUM_CONCURRENT_TESTS
+
+# Build the arguments for download the test warehouse and metastore snapshots.
+DOWNLOAD_SNAPSHOT_ARGS=("--warehouse_snapshot_dir=${TEST_WAREHOUSE_SNAPSHOT_DIR}")
+DOWNLOAD_SNAPSHOT_ARGS+=("--metastore_snapshot_dir=${METASTORE_SNAPSHOT_DIR}")
+if [ -n "${DATA_LOAD_BUILD_NAME}" ] ; then
+  DOWNLOAD_SNAPSHOT_ARGS+=("--jenkins_job_name=${DATA_LOAD_BUILD_NAME}")
+fi
+DOWNLOAD_SNAPSHOT_ARGS+=("--clean")
+
+# Download the appropriate snapshot if required, and set IMPALA_SNAPSHOT_FILE to its
+# location. If a snapshot exists that's different from the one we want to download, delete
+# it.
+pushd ${IMPALA_BUILD_SCRIPTS_HOME}/jenkins
+if [[ "${SKIP_FULL_DATA_LOAD}" = "true" ]]; then
+  if ! ./download-latest-snapshot.py ${DOWNLOAD_SNAPSHOT_ARGS[@]}; then
+    echo "Unable to download snapshots, aborting build"
+    exit 1
+  fi
+fi
+popd
+
+# Find the snapshot (may be empty if one was not downloaded)
+export IMPALA_SNAPSHOT_FILE=\
+`find $WORKSPACE/testdata/test-warehouse-SNAPSHOT/ -name test-warehouse-*-SNAPSHOT.tar.gz`
+# Find the metastore snapshot (maybe empty if one was not downloaded)
+export METASTORE_SNAPSHOT_FILE=\
+`find ${METASTORE_SNAPSHOT_DIR}/ -name hive_impala_dump*.txt`
+
+echo ">>> Shell environment:"
+env
+
+echo
+echo
+echo "********************************************************************************"
+echo " Environment setup complete, build proper follows"
+echo "********************************************************************************"
+echo
+echo
